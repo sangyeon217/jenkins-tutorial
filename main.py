429c5ee8a070613ed33994b86afec0dbcf7a70fb
sangyeon217 <sangyeon217@gmail.com>
2022-02-08 21:04:52 +0900

    Jenkins Console Output 에 로그 스트림 출력이 남는지 추가 확인

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ if __name__ == '__main__':
     logging_config.init()
     logger = logging.getLogger()
     logger.error(msg='Jenkins Console Output 에 로그 스트림 출력이 남는지 확인')
+    logger.debug(msg='DEBUG 로그 메세지')
+    logger.info(msg='INFO 로그 메세지')
+    logger.warning(msg='WARNING 로그 메세지')
