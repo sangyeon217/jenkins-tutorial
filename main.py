41358f3810abc2f11c9a0663f4449d9ea8819236
sangyeon217 <sangyeon217@gmail.com>
2022-02-12 10:20:36 +0900

    파일 저장 테스트

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import logging
 import logging_config
+import csv
+logging_config.init()
+logger = logging.getLogger()
 
-if __name__ == '__main__':
-    logging_config.init()
-    logger = logging.getLogger()
+
+def print_log():
     logger.error(msg='Jenkins Console Output 에 로그 스트림 출력이 남는지 확인')
     logger.debug(msg='DEBUG 로그 메세지')
     logger.info(msg='INFO 로그 메세지')
     logger.warning(msg='WARNING 로그 메세지')
+
+
+def save_file(file_name):
+    with open(file=file_name, mode='w', encoding='utf-8-sig', newline='') as f:
+        fw = csv.writer(f)
+        fw.writerow('TEST')
+
+
+if __name__ == '__main__':
+    print_log()
+    save_file(file_name='test.txt')
