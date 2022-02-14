d722d922113c084b21242d6ab3a3c063545e8418
sangyeon217 <sangyeon217@gmail.com>
2022-02-14 22:08:38 +0900

    user-agent 테스트

--- main.py
+++ main.py
@@ -37,9 +37,21 @@ def save_screenshot():
         driver.quit()
 
 
+def print_user_agent():
+    driver = selenium_func.set_chrome_driver(headless_option=True, secret_mode=True)
+    url = "https://github.com/sangyeon217/jenkins-tutorial"
+    try:
+        driver.get(url)
+        user_agent = driver.execute_script("return navigator.userAgent;")
+        print(user_agent)
+    finally:
+        driver.quit()
+
+
 if __name__ == '__main__':
     print_log()
     text_file = 'test.txt'
     save_file(file=text_file)
     save_html_file(file=time_func.put_datetime_in_filename(file_type='html', file=FileDirectory(directory_type='html').directory + 'example.html', include_seconds=True), html_title=file_func.convert_file_to_text(file=text_file))
     save_screenshot()
+    print_user_agent()
