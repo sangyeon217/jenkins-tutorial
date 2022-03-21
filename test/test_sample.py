b118c388fee0c7dd6d7a3ea7638c8c3727118fc9
sangyeon217 <sangyeon217@gmail.com>
2022-03-21 21:55:42 +0900

    pytest 실행 시 Console Output 로그 출력 테스트

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,4 +1,12 @@
-def test_sample1():
+import main
+main.print_log()
+
+
+def test_sample1(test_logger):
+    test_logger.debug(msg='Passed Test: DEBUG LOG MESSAGE')
+    test_logger.info(msg='Passed Test: INFO LOG MESSAGE')
+    test_logger.warning(msg='Passed Test: WARNING LOG MESSAGE')
+    test_logger.error(msg='Passed Test: ERROR LOG MESSAGE')
     assert True
 
 
